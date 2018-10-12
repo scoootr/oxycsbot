f3ebb8b6870b584bac3190694e57f480814d0545
Justin Li <justinnhli@gmail.com>
2018-10-12 00:53:03 -0700

    add documentation

--- oxycsbot.py
+++ oxycsbot.py
@@ -1,14 +1,57 @@
 #!/usr/bin/env python3
 
+"""A tag-based chatbot framework."""
+
 import re
 from collections import Counter
 
 class ChatBot:
+    """A tag-based chatbot framework
+
+    This class is not meant to be instantiated. Instead, it provides helper
+    functions that subclasses could use to create a tag-based chatbot. There
+    are two main components to a chatbot:
+
+    * A set of STATES to determine the context of a message.
+    * A set of TAGS that match on words in the message.
+
+    Subclasses must implement two methods for every state (except the
+    default): the `on_enter_*` method and the `respond_from_*` method. For
+    example, if there is a state called "confirm_delete", there should be two
+    methods `on_enter_confirm_delete` and `respond_from_confirm_delete`.
+
+    * `on_enter_*()` is what the chatbot should say when it enters a state.
+        This method takes no arguments, and returns a string that is the
+        chatbot's response. For example, a bot might enter the "confirm_delete"
+        state after a message to delete a reservation, and the
+        `on_enter_confirm_delete` might return "Are you sure you want to
+        delete?".
+
+    * `respond_from_*()` determines which state the chatbot should enter next.
+        It takes two arguments: a string `message`, and a dictionary `tags`
+        which counts the number of times each tag appears in the message. This
+        function should always return with calls to either `go_to_state` or
+        `finish`.
+
+    The `go_to_state` method automatically calls the related `on_enter_*`
+    method before setting the state of the chatbot. The `finish` function calls
+    a `finish_*` function before setting the state of the chatbot to the
+    default state.
+
+    The TAGS class variable is a dictionary whose keys are words/phrases and
+    whose values are (list of) tags for that word/phrase. If the words/phrases
+    match a message, these tags are provided to the `respond_from_*` methods.
+    """
 
     STATES = []
     TAGS = {}
 
     def __init__(self, default_state):
+        """Initialize a Chatbot.
+
+        Arguments:
+            default_state (str): The starting state of the agent.
+        """
         if default_state not in self.STATES:
             print(' '.join([
                 f'WARNING:',
@@ -22,6 +65,7 @@ class ChatBot:
         self._check_tags()
 
     def _check_states(self):
+        """Check the STATES to make sure that relevant functions are defined."""
         for state in self.STATES:
             prefixes = []
             if state != self.default_state:
@@ -36,6 +80,7 @@ class ChatBot:
                     ]))
 
     def _check_tags(self):
+        """Check the TAGS to make sure that it has the correct format."""
         for phrase in self.TAGS:
             tags = self.TAGS[phrase]
             if isinstance(tags, str):
@@ -48,6 +93,14 @@ class ChatBot:
             ])
 
     def go_to_state(self, state):
+        """Set the chatbot's state after responding appropriately.
+
+        Arguments:
+            state (str): The state to go to.
+
+        Returns:
+            str: The response of the chatbot.
+        """
         assert state in self.STATES, f'ERROR: state "{state}" is not defined'
         assert state != self.default_state, ' '.join([
             'WARNING:',
@@ -60,6 +113,7 @@ class ChatBot:
         return response
 
     def chat(self):
+        """Start a chat with the chatbot."""
         try:
             message = input('> ')
             while message.lower() not in ('exit', 'quit'):
@@ -73,15 +127,41 @@ class ChatBot:
 
 
     def respond(self, message):
+        """Respond to a message.
+
+        Arguments:
+            message (str): The message from the user.
+
+        Returns:
+            str: The response of the chatbot.
+        """
         respond_method = getattr(self, f'respond_from_{self.state}')
         return respond_method(message, self._get_tags(message))
 
     def finish(self, manner):
+        """Set the chatbot back to the default state
+
+        This function will call the appropriate `finish_*` method.
+
+        Arguments:
+            manner (str): The type of exit from the flow.
+
+        Returns:
+            str: The response of the chatbot.
+        """
         response = getattr(self, f'finish_{manner}')()
         self.state = self.default_state
         return response
 
     def _get_tags(self, message):
+        """Find all tagged words/phrases in a message.
+
+        Arguments:
+            message (str): The message from the user.
+
+        Returns:
+            Dict[str, int]: A count of each tag found in the message.
+        """
         counter = Counter()
         msg = message.lower()
         for phrase, tags in self.TAGS.items():
@@ -91,6 +171,7 @@ class ChatBot:
 
 
 class OxyCSBot(ChatBot):
+    """A simple chatbot that directs students to office hours of CS professors."""
 
     STATES = [
         'waiting',
@@ -133,10 +214,23 @@ class OxyCSBot(ChatBot):
     ]
 
     def __init__(self):
+        """Initialize the OxyCSBot.
+
+        The `professor` member variable stores whether the target
+        professor has been identified.
+        """
         super().__init__(default_state='waiting')
         self.professor = None
 
     def get_office_hours(self, professor):
+        """Find the office hours of a professor.
+
+        Arguments:
+            professor (str): The professor of interest.
+
+        Returns:
+            str: The office hours of that professor.
+        """
         office_hours = {
             'celia': 'F 12-1:45pm; F 2:45-4:00pm',
             'hsing-hau': 'T 1-2:30pm; Th 10:30am-noon',
@@ -147,6 +241,14 @@ class OxyCSBot(ChatBot):
         return office_hours[professor]
 
     def get_office(self, professor):
+        """Find the office of a professor.
+
+        Arguments:
+            professor (str): The professor of interest.
+
+        Returns:
+            str: The office of that professor.
+        """
         office = {
             'celia': 'Swan 216',
             'hsing-hau': 'Swan 302',
@@ -155,6 +257,8 @@ class OxyCSBot(ChatBot):
             'kathryn': 'Swan B101',
         }
         return office[professor]
+
+    # "waiting" state functions
 
     def respond_from_waiting(self, message, tags):
         self.professor = None
@@ -169,6 +273,8 @@ class OxyCSBot(ChatBot):
         else:
             return self.finish('confused')
 
+    # "specific_faculty" state functions
+
     def on_enter_specific_faculty(self):
         response = '\n'.join([
             f"{self.professor.capitalize()}'s office hours are {self.get_office_hours(self.professor)}",
@@ -182,6 +288,8 @@ class OxyCSBot(ChatBot):
         else:
             return self.finish('location')
 
+    # "unknown_faculty" state functions
+
     def on_enter_unknown_faculty(self):
         return "Who's office hours are you looking for?"
 
@@ -191,6 +299,8 @@ class OxyCSBot(ChatBot):
                 self.professor = professor
                 return self.go_to_state('specific_faculty')
         return self.go_to_state('unrecognized_faculty')
+
+    # "unrecognized_faculty" state functions
 
     def on_enter_unrecognized_faculty(self):
         return ' '.join([
@@ -204,6 +314,8 @@ class OxyCSBot(ChatBot):
                 self.professor = professor
                 return self.go_to_state('specific_faculty')
         return self.finish('fail')
+
+    # "finish" functions
 
     def finish_confused(self):
         return "Sorry, I'm just a simple bot that understands a few things. You can ask me about office hours though!"
