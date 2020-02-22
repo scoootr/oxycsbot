faf5ea40a5c38a1e116723a483b1aa4ff639c0f0
Justin Li <justinnhli@gmail.com>
2020-02-22 11:39:10 -0800

    revert to a dictionary for global state
    Reverts 94d176a and 7192640. I'm not fully sure why the SimpleNamespace
    doesn't work.

--- slackbot.py
+++ slackbot.py
@@ -3,7 +3,6 @@
 
 import ssl
 from os import environ
-from types import SimpleNamespace
 
 import certifi
 from flask import Flask
@@ -19,15 +18,17 @@ app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
 slack_web_client = WebClient(token=environ['SLACK_BOT_TOKEN'])
 
-global_state = SimpleNamespace()
-
+global_state = {
+    'bot_id': slack_web_client.auth_test()['user_id'],
+    'partners': {},
+}
 
 @slack_events_adapter.on('message')
 def message(payload):
     """Pass messages onto a per user, per channel chatbot instance."""
     event = payload.get('event', {})
 
-    bot_id = global_state.bot_id
+    bot_id = global_state['bot_id']
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
@@ -36,13 +37,13 @@ def message(payload):
     if user_id == bot_id:
         return
     # ignore messages that don't mention the bot
-    if f'<@{global_state.bot_id}>' not in text:
+    if f'<@{global_state["bot_id"]}>' not in text:
         return
 
     key = (user_id, channel_id)
-    if key not in global_state.partners:
-        global_state.partners[key] = OxyCSBot() # FIXME replace with your chatbot class
-    chatbot = global_state.partners[key]
+    if key not in global_state['partners']:
+        global_state['partners'][key] = OxyCSBot() # FIXME replace with your chatbot class
+    chatbot = global_state['partners'][key]
     response = chatbot.respond(text)
 
     slack_web_client.chat_postMessage(
@@ -53,8 +54,6 @@ def message(payload):
 
 def main():
     ssl_context = ssl.create_default_context(cafile=certifi.where())
-    global_state.bot_id = slack_web_client.auth_test()['user_id']
-    global_state.partners = {}
     app.run()
 
 
