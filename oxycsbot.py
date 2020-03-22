6304c6edbfb610c291cd0996c6f5c70df78d3b96
nelsonco <CourtneyA.Nelson1@gmail.com>
2020-03-22 15:47:12 -0700

    Update

--- oxycsbot.py
+++ oxycsbot.py
@@ -222,8 +222,15 @@ class OxyCSBot(ChatBot):
     def respond_from_start_interview(self,message,tags):
         return self.finish('success')
 
+    def on_enter_experience(self):
+        return "Do you have any work experience or extracurriculars?"
+    def respond_from_experience(self,message,tags):
+        return self.go_to_state('experience_feedback'):
 
-
+    def on_enter_experience_feedback(self):
+        return "When talking about your past experiences, make sure to reference specific positions and skills relevant to the position you are applying for. However, be honest with your answer. Any experience counts! If you have little to no paid work experience, include some valuable extracurriculars. "
+    def return_from_experience_feedback(self,message,tags):
+        return self.go_to_state('unknown_faculty')
 
     def on_enter_unknown_faculty(self):
         """Send a message when entering the "unknown_faculty" state."""
