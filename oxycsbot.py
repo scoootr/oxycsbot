995b54c0cd2d2dff71e55763d2b4578c14478bb1
nelsonco <CourtneyA.Nelson1@gmail.com>
2020-03-22 16:32:18 -0700

    Update

--- oxycsbot.py
+++ oxycsbot.py
@@ -231,17 +231,7 @@ class OxyCSBot(ChatBot):
     def on_enter_challenge(self):
         return "Describe a time you were struggling with a challenge. How did you overcome it and what did you learn?"
     def respond_from_challenge(self,message,tags):
-        return self.go_to_state('challenge_feedback'):
-
-    def on_enter_challenge(self):
-        return "(One important thing to remember about this question is that you want to make sure that this challenge adds to your sense of person or adds to the interviewer’s perception of you. What does your experience with this challenge and how you overcame it tell the interviewer about you?)"
-    def respond_from_challenge_feedback(self,message,tags):
-        return self.go_to_state('unknown_faculty')
-
-    def on_enter_weaknesses(self):
-        return "What is a weakness that you have?"
-    def respond_from_weakness(self,message,tags):
-        return self.go_to_state('unknown_faculty')
+        return self.go_to_state('unknown_faculty'):
 
     def on_enter_unknown_faculty(self):
         """Send a message when entering the "unknown_faculty" state."""
